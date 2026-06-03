fe71246e57fa853771f3758e8ed65fb01b13b000
meetmayani030 <meetmayani2006@gmail.com>
2026-06-03 16:14:55 +0530

    to deploy first react

--- src/App.js
+++ src/App.js
@@ -4,11 +4,11 @@ import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import React , { useState } from 'react';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route
-} from "react-router-dom";
+// import {
+//   BrowserRouter as Router,
+//   Routes,
+//   Route
+// } from "react-router-dom";
 function App() {
   const [mode,setMode]=useState('light');
   const [alert,setAlert]=useState();
@@ -40,31 +40,23 @@ function App() {
     }
   }
   return (
-    <Router>
+    // <Router>
     <>
       {/* <Navbar title="TextUtils2vvbhivub" sectitle="About my self"/> */}
       <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode}/>
       <Alert alert={alert}/>
       <div className="container my-3">
         
-          <Routes>
-  <Route exact path="/about" element={<About />} />
-
-          <Route
+          {/* <Routes> */}
+  {/* <Route exact path="/about" element={<About />} /> */}
+          {/* <Route
             exact path="/"
-            element={
-              <TextForm
-                showAlert={showAlert}
-                heading="Enter the text to analyze"
-                mode={mode}
-              />
-            }
-          />
-        </Routes>
-        
+            element={ */}
+              <TextForm showAlert={showAlert}  heading="Enter the text to analyze"  mode={mode}  />
+        {/* </Routes> */}
       </div>
     </>
-    </Router>
+    /* </Router> */
   );
 }
 export default App;
